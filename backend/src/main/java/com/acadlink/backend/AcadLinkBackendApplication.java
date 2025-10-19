7d070ba34a7eac1578ec8110baab350bac2c73c7
parsjan <parasjain230620@gmail.com>
2025-10-19 17:10:35 +0530

    basic setup of project

---
+++ backend/src/main/java/com/acadlink/backend/AcadLinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acadlink.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadLinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadLinkBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/acadlink/backend/AcadLinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acadlink.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadLinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
